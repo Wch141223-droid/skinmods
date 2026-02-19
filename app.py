deaea86f0398e830177ccfecb7588b8ba99c4021
Wch141223-droid <Wch141223@qq.com>
2026-02-19 13:00:59 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,15 @@
 import os
-from flask import Flask, render_template, request, redirect, url_for, flash
+from flask import Flask, render_template, request, redirect, url_for, flash, send_from_directory
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 from flask_mail import Mail, Message
-from werkzeug.security import generate_password_hash, check_password_hash
 from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 app.secret_key = os.environ.get("SECRET_KEY")
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///skinmods.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['UPLOAD_FOLDER'] = 'static/skins'
-app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
@@ -21,24 +19,18 @@ app.config['MAIL_PASSWORD'] = os.environ.get("EMAIL_PASS")
 db = SQLAlchemy(app)
 login_manager = LoginManager()
 login_manager.init_app(app)
-login_manager.login_view = 'login'
 mail = Mail(app)
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(150), unique=True)
     email = db.Column(db.String(150), unique=True)
     password = db.Column(db.String(150))
 
-class Skin(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    filename = db.Column(db.String(300))
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-
 class Comment(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(150))
     content = db.Column(db.Text)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    skin_id = db.Column(db.Integer, db.ForeignKey('skin.id'))
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -46,22 +38,25 @@ def load_user(user_id):
 
 @app.route('/')
 def index():
-    skins = Skin.query.all()
     comments = Comment.query.all()
-    return render_template('index.html', skins=skins, comments=comments)
+    return render_template('index.html', comments=comments)
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     if request.method == 'POST':
+        username = request.form['username']
         email = request.form['email']
         password = request.form['password']
         if User.query.filter_by(email=email).first():
             flash('邮箱已注册')
             return redirect(url_for('register'))
-        new_user = User(email=email, password=generate_password_hash(password, method='sha256'))
+        new_user = User(username=username, email=email, password=password)
         db.session.add(new_user)
         db.session.commit()
-        flash('注册成功')
+        msg = Message('注册成功', sender=app.config['MAIL_USERNAME'], recipients=[email])
+        msg.body = f'{username}，恭喜你注册成功！'
+        mail.send(msg)
+        flash('注册成功，请登录')
         return redirect(url_for('login'))
     return render_template('register.html')
 
@@ -70,12 +65,11 @@ def login():
     if request.method == 'POST':
         email = request.form['email']
         password = request.form['password']
-        user = User.query.filter_by(email=email).first()
-        if not user or not check_password_hash(user.password, password):
-            flash('邮箱或密码错误')
-            return redirect(url_for('login'))
-        login_user(user)
-        return redirect(url_for('index'))
+        user = User.query.filter_by(email=email, password=password).first()
+        if user:
+            login_user(user)
+            return redirect(url_for('index'))
+        flash('邮箱或密码错误')
     return render_template('login.html')
 
 @app.route('/logout')
@@ -84,36 +78,34 @@ def logout():
     logout_user()
     return redirect(url_for('index'))
 
-@app.route('/upload', methods=['POST'])
+@app.route('/upload_skin', methods=['POST'])
 @login_required
-def upload():
+def upload_skin():
     if 'skin' not in request.files:
-        flash('没有文件')
+        flash('未选择文件')
         return redirect(url_for('index'))
     file = request.files['skin']
     if file.filename == '':
-        flash('请选择文件')
+        flash('未选择文件')
         return redirect(url_for('index'))
     filename = secure_filename(file.filename)
-    filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-    file.save(filepath)
-    new_skin = Skin(filename=filename, user_id=current_user.id)
-    db.session.add(new_skin)
-    db.session.commit()
-    flash('上传成功')
+    file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+    flash('皮肤上传成功')
     return redirect(url_for('index'))
 
-@app.route('/comment/<int:skin_id>', methods=['POST'])
+@app.route('/comment', methods=['POST'])
 @login_required
-def comment(skin_id):
+def comment():
     content = request.form['content']
-    new_comment = Comment(content=content, user_id=current_user.id, skin_id=skin_id)
+    new_comment = Comment(username=current_user.username, content=content)
     db.session.add(new_comment)
     db.session.commit()
-    flash('评论已提交')
     return redirect(url_for('index'))
 
+@app.route('/skins/<filename>')
+def skins(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+
 if __name__ == '__main__':
-    os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
     db.create_all()
     app.run(host='0.0.0.0', port=8080)
