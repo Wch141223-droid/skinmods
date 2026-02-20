c1bf2ef1628d823ec3b64a78c292e1c17c5226a1
Wch141223-droid <Wch141223@qq.com>
2026-02-20 11:14:32 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,18 @@
+import os
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secret'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config["SECRET_KEY"] = "supersecretkey"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
-db = SQLAlchemy(app)
-login_manager = LoginManager(app)
+db = SQLAlchemy()
+db.init_app(app)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
 login_manager.login_view = "login"
 
 class User(UserMixin, db.Model):
@@ -22,55 +26,57 @@ class Comment(db.Model):
     content = db.Column(db.String(500), nullable=False)
 
 with app.app_context():
-    db.drop_all()
     db.create_all()
 
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route("/", methods=["GET", "POST"])
 def index():
-    if request.method == 'POST':
+    if request.method == "POST":
         if current_user.is_authenticated:
-            content = request.form.get('content')
+            content = request.form.get("content")
             if content:
-                comment = Comment(username=current_user.username, content=content)
-                db.session.add(comment)
+                new_comment = Comment(username=current_user.username, content=content)
+                db.session.add(new_comment)
                 db.session.commit()
-        return redirect(url_for('index'))
+        return redirect(url_for("index"))
     comments = Comment.query.all()
-    return render_template('index.html', comments=comments)
+    return render_template("index.html", comments=comments)
 
-@app.route('/login', methods=['GET', 'POST'])
+@app.route("/login", methods=["GET", "POST"])
 def login():
-    if request.method == 'POST':
-        username = request.form.get('username')
-        password = request.form.get('password')
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
         user = User.query.filter_by(username=username, password=password).first()
         if user:
             login_user(user)
-            return redirect(url_for('index'))
-    return render_template('login.html')
+            return redirect(url_for("index"))
+    return render_template("login.html")
 
-@app.route('/register', methods=['GET', 'POST'])
+@app.route("/register", methods=["GET", "POST"])
 def register():
-    if request.method == 'POST':
-        username = request.form.get('username')
-        password = request.form.get('password')
-        if username and password and not User.query.filter_by(username=username).first():
-            user = User(username=username, password=password)
-            db.session.add(user)
-            db.session.commit()
-            login_user(user)
-            return redirect(url_for('index'))
-    return render_template('register.html')
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+        if username and password:
+            existing = User.query.filter_by(username=username).first()
+            if not existing:
+                new_user = User(username=username, password=password)
+                db.session.add(new_user)
+                db.session.commit()
+                login_user(new_user)
+                return redirect(url_for("index"))
+    return render_template("register.html")
 
-@app.route('/logout')
+@app.route("/logout")
 @login_required
 def logout():
     logout_user()
-    return redirect(url_for('index'))
+    return redirect(url_for("index"))
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
